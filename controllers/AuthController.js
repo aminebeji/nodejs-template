4eab62aacf2c570305df7f86d12ccdc7bfd55056
amine.beji <amine.beji001@gmail.com>
2022-09-17 21:18:30 +0100

    configure docker-compose / fix server and DB errors :bug:

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,7 +3,7 @@ const { FindUserById } = require("../repository/UserRepo");
 const getUser = async (req, res) => {
   FindUserById(req.params.id, (result) => {
     if (result.error) {
-      return res.status(400).json({ message: "ERROR in Get User !" });
+      return res.status(400).json({ message: "ERROR in Get User !" , error : result });
     }
     return res.status(200).json(result);
   });
